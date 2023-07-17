2834c6e95ecde43946a00480de203667095e4e5c
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-07-18 00:17:30 +0300

    refactor main test commands

--- bin/bond_cli.dart
+++ bin/bond_cli.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
 
+import 'package:args/command_runner.dart';
 import 'package:bond_cli/commands/bond_command_runner.dart';
 
 void main(List<String> arguments) async {
-  try {
-    await BondCommandRunner().run(
-      [
-        'create',
-        '--project_name',
-        'my_project',
-      ],
-    );
-  } catch (err) {
-    stderr.writeln(err.toString());
-    rethrow;
-  }
+  await BondCommandRunner().run(
+    [
+      'create',
+      '--project_name',
+      'my_project',
+    ],
+  ).catchError((error) {
+    if (error is! UsageException) throw error;
+    print(error);
+    stderr.writeln(error.toString());
+    exit(64); // Exit code 64 indicates a software error.
+  });
 }
