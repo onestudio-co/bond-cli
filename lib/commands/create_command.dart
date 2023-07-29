8c9240cb9b194cdfccdde89ebdf3655b6d0941ef
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-07-30 02:07:26 +0300

    make create command and addSubcommand

---
+++ lib/commands/create_command.dart
@@ -0,0 +1,19 @@
+import 'package:args/command_runner.dart';
+import 'package:bond_cli/commands/create_project/create_project_command.dart';
+
+import 'create_feature/commands/create_model_command.dart';
+
+class CreateCommand extends Command {
+  @override
+  String get description => 'Creates a new Flutter Bond component';
+
+  @override
+  String get name => 'create';
+
+  CreateCommand() {
+    addSubcommand(CreateProjectCommand());
+    addSubcommand(CreateModelCommand());
+
+    // Add more subcommands as needed...
+  }
+}
