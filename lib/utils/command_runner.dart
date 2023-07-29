9b42d81e8dc2e4cfd5105431b5965817af324f22
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-07-29 13:06:59 +0300

    handle error when runBuildRunner

--- lib/utils/command_runner.dart
+++ lib/utils/command_runner.dart
@@ -5,15 +5,20 @@ import 'package:bond_cli/utils/print_utils.dart';
 class CommandRunnerHelper {
   /// Run the build runner for the given [path].
   static void runBuildRunner(String path) async {
-    var result = await Process.run('dart', [
-      'run',
-      'build_runner',
-      'build',
-      '--delete-conflicting-outputs',
-      '--build-filter="$path*.dart"'
-    ]);
+    try {
+      var result = await Process.run('dart', [
+        'run',
+        'build_runner',
+        'build',
+        '--delete-conflicting-outputs',
+        '--build-filter="$path*.dart"'
+      ]);
 
-    ConsolePrinter.success(result.stdout);
-    ConsolePrinter.success(result.stderr);
+      ConsolePrinter.success(result.stdout);
+      ConsolePrinter.error(result.stderr);
+    } catch (err) {
+      ConsolePrinter.error(err.toString());
+      rethrow;
+    }
   }
 }
