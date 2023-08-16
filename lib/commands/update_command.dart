f3aeb40c9cbc75fd00429f0bae63a716c4e82cdf
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-08-17 02:26:03 +0300

    add UpdateCommand

---
+++ lib/commands/update_command.dart
@@ -0,0 +1,17 @@
+import 'package:args/command_runner.dart';
+import 'package:bond_cli/commands/create_project/update_app_name_command.dart';
+
+import 'create_project/update_ios_bundle_id_command.dart';
+
+class UpdateCommand extends Command {
+  @override
+  String get description => 'Update an exist Flutter Bond component';
+
+  @override
+  String get name => 'update';
+
+  UpdateCommand() {
+    addSubcommand(UpdateAppNameCommand());
+    addSubcommand(UpdateIOSBundleIdCommand());
+  }
+}
