6a7350fcab150db5d7b324cb732a55e63a223a29
Qamar Safadi <qamar.elsafadi98@gmail.com>
2023-09-23 18:29:35 +0300

    feat: replace CommandRunner to BondCommandRunner

--- bin/bond.dart
+++ bin/bond.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
-import 'package:args/command_runner.dart';
-import 'package:bond_cli/core/tasks/github_example/command/GithubExampleCommand.dart';
+import 'package:bond_cli/commands/bond_command_runner.dart';
+import 'package:bond_cli/core/tasks/github_example/command/github_example_command.dart';
 
 //  dart compile exe bin/bond.dart -o bond_cli
 // /Users/salahamassi/FlutterProjects/plugins/bond_cli/bond_cli
 void main(List<String> arguments) async {
-  final app = CommandRunner('bond', 'Bond CLI Description')
-    ..addCommand(MySampleCommand());
+  final app = BondCommandRunner()
+    ..addCommand(GithubExampleCommand());
   app.run(arguments);
 }
