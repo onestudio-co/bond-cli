766ed5a778fc82d698c08562a382e0e25a287e37
Salah Nahed <salahamassi@users.noreply.github.com>
2024-07-28 02:09:39 +0300

    feat: add help message for local development version of BOND CLI

--- bin/bond.dart
+++ bin/bond.dart
@@ -2,6 +2,17 @@ import 'package:bond_cli/commands/bond_command_runner.dart';
 import 'package:bond_cli/core/utils.dart';
 
 void main(List<String> arguments) async {
+  if (arguments.contains('--help')) {
+    // ignore_for_file: avoid_print
+    ConsolePrinter.warning(
+        '------------------------------------------------------------------');
+    ConsolePrinter.warning(
+      '⚠️ You are running a local development version of BOND CLI. ⚠️',
+    );
+    ConsolePrinter.warning(
+        '------------------------------------------------------------------');
+    ConsolePrinter.warning('');
+  }
   try {
     await BondCommandRunner().run(arguments);
   } catch (err) {
